09f721b389393ce46872c2e4dd9fcb90b1868cc1
TUdayKiranReddy <53467166+TUdayKiranReddy@users.noreply.github.com>
2020-11-07 10:13:15 +0530

    Phase asymmtotes updated!!

--- bode_plotter.py
+++ bode_plotter.py
@@ -11,11 +11,19 @@ def first_order(tau):
 	w1 = w[w > 1/tau]
 	mag0 = (np.zeros(w0.shape))
 	mag1 = (-20*np.log10(tau*w1))
-
+	
+	pw0 = w[w <= factor/tau]
+	pwi = w[(factor/tau < w)*(w <= 1/(tau*factor))]
+	pw1 = w[w > 1/(tau*factor)]
+	phi_0 = np.zeros(pw0.shape)
+	phi_i = 45*(np.log10(pwi*tau/factor))/np.log10(factor)
+	phi_1 = -90*np.ones(pw1.shape)
+	
 	bode = dict()
 	bode['mag'] = mag
 	bode['mag_assym'] = np.append(mag0, mag1)
 	bode['phi'] = phi
+	bode['phi_assym'] = np.append(phi_0, np.append(phi_i, phi_1))
 
 	return bode
 
@@ -31,11 +39,19 @@ def secound_order(eta, wn):
 	w1 = w[w > wn]
 	mag0 = (np.zeros(w0.shape))
 	mag1 = (-40*np.log10(w1/wn))
+
+	pw0 = w[w <= factor*wn]
+	pwi = w[(factor*wn < w)*(w <= wn/(factor))]
+	pw1 = w[w > wn/(factor)]
+	phi_0 = np.zeros(pw0.shape)
+	phi_i = 90*(np.log10(pwi/(factor*wn)))/np.log10(factor)
+	phi_1 = -180*np.ones(pw1.shape)
 	
 	bode = dict()
 	bode['mag'] = mag
 	bode['mag_assym'] = np.append(mag0, mag1)
 	bode['phi'] = phi
+	bode['phi_assym'] = np.append(phi_0, np.append(phi_i, phi_1))
 
 	return bode
 
@@ -45,29 +61,40 @@ def add(bode1, bode2):
 	bode['mag'] = bode1['mag'] + bode2['mag']
 	bode['mag_assym'] =  bode1['mag_assym'] + bode2['mag_assym']
 	bode['phi'] = bode1['phi'] + bode2['phi']
+	bode['phi_assym'] = bode1['phi_assym'] + bode2['phi_assym']
 	
 	return bode
 
-def factor(k, bode_raw):
+def sub(bode1, bode2):
+	bode = dict()
+	
+	bode['mag'] = bode1['mag'] - bode2['mag']
+	bode['mag_assym'] =  bode1['mag_assym'] - bode2['mag_assym']
+	bode['phi'] = bode1['phi'] - bode2['phi']
+	bode['phi_assym'] = bode1['phi_assym'] - bode2['phi_assym']
+	
+	return bode
+
+def constant_factor(k, bode_raw):
 	bode = dict()
 	
 	bode['mag'] = 20*np.log10(k) + bode_raw['mag']
 	bode['mag_assym'] = 20*np.log10(k) + bode_raw['mag_assym']
 	bode['phi'] = bode_raw['phi']
+	bode['phi_assym'] = bode_raw['phi_assym']
 	
 	return bode
 	
-w = np.linspace(1e-2, 1e3, 1e6)
+w = np.linspace(1e-2, 1e3, 1e6)			### omega range
+factor = 0.2							### factor for phase assymptotes
 
-
-bode = add(first_order(0.05), secound_order(0.1, 100))
-#bode = add(bode, secound_order(0.1, 100))
-
-bode = factor(1, bode)
+bode = sub(secound_order(0.1, 100), first_order(0.5))			## TF = (s/2 + 1)(1e4)/(s^2 + 20s + 1e4)
+bode = constant_factor(1, bode)
 
 mag = bode['mag']
 mag_assym = bode['mag_assym']
 phi = bode['phi']
+phi_assym = bode['phi_assym']
 
 
 plt.figure()
@@ -80,6 +107,7 @@ plt.grid()
 
 plt.figure()
 plt.semilogx(w, phi)
+plt.semilogx(w, phi_assym, '--g')
 plt.title('Phase('+r'$\phi$'+')')
 plt.xlabel(r'$\omega$')
 plt.ylabel('o')
